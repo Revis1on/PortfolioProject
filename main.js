4d6e2b0869597125ade6fae9ac47c5039aab1f2c
Filip Risteski <filip22risteski@gmail.com>
2021-01-05 23:05:00 +0100

    ver 1.5

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-$('.top-nav .nav-link').on('click', function () {
-    $('.menu-toggler').removeClass('open');
-    $('.top-nav').removeClass('open');
-});
-
 $('.nav a[href*="#"]').on('click', function () {
   $('html, body').animate( {
       scrollTop: $($(this).attr('href')).offset().top - 100
